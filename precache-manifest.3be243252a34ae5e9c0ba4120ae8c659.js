60e8132cec8f18c4c462722f22d8a50a3b4c0ccc
Timothy Choe <timothychoe@MacBook-Pro.local>
2020-06-30 17:18:37 -0400

    Updates

---
+++ precache-manifest.3be243252a34ae5e9c0ba4120ae8c659.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42c9cc422801d15c3ae9502f712b412a",
+    "url": "/URL-shortening-API-landing-page/index.html"
+  },
+  {
+    "revision": "5daf8ee519c4fbdf9100",
+    "url": "/URL-shortening-API-landing-page/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5daf8ee519c4fbdf9100",
+    "url": "/URL-shortening-API-landing-page/static/js/2.32dd5bcc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/URL-shortening-API-landing-page/static/js/2.32dd5bcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f909a6a96848bfc0811",
+    "url": "/URL-shortening-API-landing-page/static/js/main.9e619431.chunk.js"
+  },
+  {
+    "revision": "257cebd40e18eec78af2",
+    "url": "/URL-shortening-API-landing-page/static/js/runtime-main.e5b693cb.js"
+  },
+  {
+    "revision": "1922b68a4d1dce29b2c40904cdbc1bba",
+    "url": "/URL-shortening-API-landing-page/static/media/icon-brand-recognition.1922b68a.svg"
+  },
+  {
+    "revision": "e3d9caa9df26426d598106f7f7289625",
+    "url": "/URL-shortening-API-landing-page/static/media/icon-detailed-records.e3d9caa9.svg"
+  },
+  {
+    "revision": "7febcff9f143b83553d544fc04fc5b09",
+    "url": "/URL-shortening-API-landing-page/static/media/icon-facebook.7febcff9.svg"
+  },
+  {
+    "revision": "cc099cea0da2f5ae13aef182c107a854",
+    "url": "/URL-shortening-API-landing-page/static/media/icon-fully-customizable.cc099cea.svg"
+  },
+  {
+    "revision": "996b697c33ab8772e59e3c9bec1124ea",
+    "url": "/URL-shortening-API-landing-page/static/media/icon-instagram.996b697c.svg"
+  },
+  {
+    "revision": "c557c3d4e80b5eda1ae120c8c2257e26",
+    "url": "/URL-shortening-API-landing-page/static/media/icon-pinterest.c557c3d4.svg"
+  },
+  {
+    "revision": "cb4fea0b4771a5dfc70f787d5e01c91e",
+    "url": "/URL-shortening-API-landing-page/static/media/icon-twitter.cb4fea0b.svg"
+  },
+  {
+    "revision": "5029d0493c803aac6c96e13be419ecb7",
+    "url": "/URL-shortening-API-landing-page/static/media/illustration-working.5029d049.svg"
+  },
+  {
+    "revision": "6387840f9b6e8720165db8c73185bc51",
+    "url": "/URL-shortening-API-landing-page/static/media/logo.6387840f.svg"
+  },
+  {
+    "revision": "40db2b9bb7de770270ceec94d4c0211b",
+    "url": "/URL-shortening-API-landing-page/static/media/logoWhite.40db2b9b.svg"
+  }
+]);
